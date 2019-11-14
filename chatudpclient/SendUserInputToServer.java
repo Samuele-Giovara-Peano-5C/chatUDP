6a19d9e9a84a8a8c95d5990324fe5bfe316714e2
Samuele-Giovara-Peano-5C <56298455+Samuele-Giovara-Peano-5C@users.noreply.github.com>
2019-11-14 11:25:11 +0100

    Update SendUserInputToServer.java

--- chatudpclient/SendUserInputToServer.java
+++ chatudpclient/SendUserInputToServer.java
@@ -21,11 +21,13 @@ public class SendUserInputToServer implements Runnable {
     DatagramSocket socket;
     InetAddress address;
     int UDP_port;
+    String nome;
     
-    SendUserInputToServer(DatagramSocket socket, InetAddress address, int UDP_port) {
+    SendUserInputToServer(DatagramSocket socket, InetAddress address, int UDP_port, String nome) {
         this.socket = socket;
-        this.address = address;
-        this.UDP_port = UDP_port;
+            this.address = address;
+                this.UDP_port = UDP_port;
+                    this.nome=nome;//Aggiunta del nickname
     }
     /**
      *
@@ -34,30 +36,32 @@ public class SendUserInputToServer implements Runnable {
     public void run() {
 
         byte[] buffer;
-        String messaggio;
-        Scanner tastiera = new Scanner(System.in);
-        DatagramPacket userDatagram;
+            String messaggio;
+            Scanner tastiera = new Scanner(System.in);
+            DatagramPacket userDatagram;
 
         try {
             System.out.print("> ");
+            
             do {
-                //Leggo da tastiera il messaggio utente vuole inviare
-                messaggio = tastiera.nextLine();
-
+                //Leggo da tastiera il messaggio utente vuole inviareciac
+                     messaggio = tastiera.nextLine();
+                    messaggio= nome.concat(": "+messaggio);//Ogni volta al MEX viene concatenato il nickname per rendere riconoscibile il client nella chat
                 //Trasformo in array di byte la stringa che voglio inviare
-                buffer = messaggio.getBytes("UTF-8");
+                    buffer = messaggio.getBytes("UTF-8");
 
                 // Costruisco il datagram (pacchetto UDP) di richiesta 
                 // specificando indirizzo e porta del server a cui mi voglio collegare
                 // e il messaggio da inviare che a questo punto si trova nel buffer
-                userDatagram = new DatagramPacket(buffer, buffer.length, address, UDP_port);
+                    userDatagram = new DatagramPacket(buffer, buffer.length, address, UDP_port);
                 // spedisco il datagram
-                socket.send(userDatagram);
-            } while (messaggio.compareTo("quit") != 0); //se utente digita quit il tread termina
-        } catch (IOException ex) {
+                    socket.send(userDatagram);
+            } 
+                while (messaggio.compareTo("quit") != 0); //se utente digita quit il tread termina
+                    socket.close();
+        } 
+        catch (IOException ex) {
             Logger.getLogger(ChatUDPclient.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
-
 }
