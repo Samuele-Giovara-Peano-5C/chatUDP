58eeb656cf9898b40155ee69494b7ed286740c32
Samuele-Giovara-Peano-5C <56298455+Samuele-Giovara-Peano-5C@users.noreply.github.com>
2019-12-17 11:19:30 +0100

    Update SendUserInputToServer.java

--- chatudpclient/SendUserInputToServer.java
+++ chatudpclient/SendUserInputToServer.java
@@ -15,19 +15,17 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Prof Matteo Palitto 
+ * @author 
  */
 public class SendUserInputToServer implements Runnable {
     DatagramSocket socket;
-    InetAddress address;
-    int UDP_port;
-    String nome;
+        InetAddress address;
+        int UDP_port;
     
-    SendUserInputToServer(DatagramSocket socket, InetAddress address, int UDP_port, String nome) {
+    SendUserInputToServer(DatagramSocket socket, InetAddress address, int UDP_port) {
         this.socket = socket;
-            this.address = address;
-                this.UDP_port = UDP_port;
-                    this.nome=nome;     //Aggiunta del nickname
+        this.address = address;
+        this.UDP_port = UDP_port;
     }
     /**
      *
@@ -36,32 +34,40 @@ public class SendUserInputToServer implements Runnable {
     public void run() {
 
         byte[] buffer;
-            String messaggio;
-            Scanner tastiera = new Scanner(System.in);
-            DatagramPacket userDatagram;
+        String messaggio;
+        String nomeUtente;
+        Scanner tastiera = new Scanner(System.in);
+        DatagramPacket userDatagram;
+        String messaggioFinale ;
 
         try {
-            System.out.print("> ");
+            System.out.println("Inserisci uno username : ");
+            nomeUtente = tastiera.nextLine();
+            
             
             do {
-                //Leggo da tastiera il messaggio utente vuole inviareciac
-                     messaggio = tastiera.nextLine();
-                    messaggio= nome.concat(": "+messaggio);//Ogni volta al MEX viene concatenato il nickname per rendere riconoscibile il client nella chat
+                //Leggo da tastiera il messaggio utente vuole inviare
+                System.out.println("Inserisci il messaggio : ");
+                messaggio = tastiera.nextLine();
+                messaggioFinale="username: "+nomeUtente+" messaggio: "+messaggio;
                 //Trasformo in array di byte la stringa che voglio inviare
-                    buffer = messaggio.getBytes("UTF-8");
+                buffer =messaggioFinale.getBytes();
 
                 // Costruisco il datagram (pacchetto UDP) di richiesta 
                 // specificando indirizzo e porta del server a cui mi voglio collegare
                 // e il messaggio da inviare che a questo punto si trova nel buffer
-                    userDatagram = new DatagramPacket(buffer, buffer.length, address, UDP_port);
+                
+                userDatagram = new DatagramPacket(buffer, buffer.length, address, UDP_port);
                 // spedisco il datagram
-                    socket.send(userDatagram);
-            } 
-                while (messaggio.compareTo("quit") != 0); //se utente digita quit il tread termina
-                    socket.close();
-        } 
-        catch (IOException ex) {
+                
+                System.out.println("la ports è : "+UDP_port);
+                
+                socket.send(userDatagram);
+            } while (messaggioFinale.compareTo("quit") != 0); //se utente digita quit il tread termina
+        } catch (IOException ex) {
             Logger.getLogger(ChatUDPclient.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
+
 }
